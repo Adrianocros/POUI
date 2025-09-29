7e27ebd208b3e543ba18ca10caca57d5215b69f5
Adrianocros <adrianobianchi@outlook.com>
2025-09-29 11:54:16 -0300

    Criando componente para editar a tarefa

--- src/app/tarefas/index.ts
+++ src/app/tarefas/index.ts
@@ -4,4 +4,6 @@ export * from './tarefas.module';
 export * from './listar';
 export * from './tarefas-routing.module';
 export * from './cadastrar';
+export * from './editar';
+
 
--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -5,13 +5,15 @@ import { ListarComponent } from './listar';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CadastrarTarefaComponent } from './cadastrar';
+import { EditarTarefaComponent } from './editar';
 
 
 
 @NgModule({
   declarations: [
     ListarComponent,
-    CadastrarTarefaComponent
+    CadastrarTarefaComponent,
+    EditarTarefaComponent
   ],
   imports: [
     CommonModule,
